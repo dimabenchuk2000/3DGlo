a20b980dd69794d7162931dc25ae44363ddf3962
Benchuk Dima <dimitron400033@gmail.com>
2024-12-21 17:52:18 +0300

    Добавил анимацию при появлении модального окна

--- src/modules/modal.js
+++ src/modules/modal.js
@@ -2,8 +2,26 @@ const modal = () => {
     const popupBtn = document.querySelectorAll(".popup-btn")
     const popup = document.querySelector(".popup")
     const closeBtn = popup.querySelector(".popup-close")
+    const popupContent = popup.querySelector(".popup-content")
 
-    popupBtn.forEach(btn => btn.addEventListener("click", () => {popup.style.display = "block"}))
+    let height = 0
+    let idInterval
+
+    const openModal = function () {
+        popup.style.display = "block"
+        height += 29
+
+        idInterval = requestAnimationFrame(openModal)
+
+        if (height < 382) {
+            popupContent.style.height = `${height}px`
+        } else {
+            height = 0
+            cancelAnimationFrame(idInterval)
+        }
+    }
+
+    popupBtn.forEach(btn => btn.addEventListener("click", openModal))
 
     closeBtn.addEventListener("click", () => {popup.style.display = "none"})
 }
