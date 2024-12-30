5121294ae5ac57c4d1c7362b195eba4efb21b0f8
Benchuk Dima <dimitron400033@gmail.com>
2024-12-30 23:34:00 +0300

    Выполнил домашнее задание урока №22

--- src/modules/menu.js
+++ src/modules/menu.js
@@ -1,19 +1,26 @@
 const menu = function () {
-  const menuBtn = document.querySelector(".menu");
   const menu = document.querySelector("menu");
-  const closeBtn = menu.querySelector(".close-btn");
-  const menuItems = menu.querySelectorAll("ul>li>a");
 
-  const menuDisplay = function () {
+  const toggleMenu = function () {
     menu.classList.toggle("active-menu");
   };
 
-  menuBtn.addEventListener("click", menuDisplay);
-  closeBtn.addEventListener("click", menuDisplay);
+  menu.addEventListener("click", (e) => {
+    if (
+      e.target.classList.contains("close-btn") ||
+      e.target.matches("ul>li>a")
+    ) {
+      toggleMenu();
+    }
+  });
 
-  menuItems.forEach((menuItem) =>
-    menuItem.addEventListener("click", menuDisplay)
-  );
+  document.addEventListener("click", (e) => {
+    if (!e.target.closest("menu") && menu.classList.contains("active-menu")) {
+      toggleMenu();
+    } else if (e.target.closest(".menu")) {
+      toggleMenu();
+    }
+  });
 };
 
 export default menu;
